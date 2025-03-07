d67ecda76794bb9ec0ffee13a0c421e1f0306e57
Nikhil Dev <arrnikhil@gmail.com>
2025-03-07 10:38:17 +0530

    Restructured the classes

--- infinite-stream/src/main/java/com/cognitree/internship/infinite_stream/CircularQueue.java
+++ infinite-stream/src/main/java/com/cognitree/internship/infinite_stream/CircularQueue.java
@@ -1,48 +1,41 @@
 package com.cognitree.internship.infinite_stream;
 
 /**
- * Class to implement immutable CircularQueue
+ * Class to implement CircularQueue
+ *
  * @param <T> - Generic type
  */
-public class CircularQueue<T extends Number> {
-    private int front;
-    private int rear;
+public class CircularQueue<T> {
     private final T[] nums;
-    private final int capacity;
+    private int index;
     private int length;
 
     /**
      * Constructor to initialise CircularQueue
+     *
      * @param size - size of queue
      */
-    CircularQueue(int size){
-       this.nums = (T[]) new Number[size];
-       this.front = 0;
-       this.length = 0;
-       this.capacity = size;
-       this.length = 0;
+    public CircularQueue(int size) {
+        this.nums = (T[]) new Object[size];
+        this.index = -1;
+        this.length = 0;
     }
 
     /**
      * Function to insert numbers into the queue
+     *
      * @param element - number to be inserted
      */
-    public final T enqueue(T element){
+    public final T enqueue(T element) {
         T last = null;
-
-        if (length == capacity) {
-            last = nums[front];
-            nums[front] = element;
-
-            front = (front + 1) % capacity;
-            rear = (rear + 1) % capacity;
+        index = (index + 1) % nums.length;
+        if (length == nums.length) {
+            last = nums[index];
         } else {
-            nums[rear] = element;
-            rear = (rear + 1) % capacity;
             length++;
         }
+        nums[index] = element;
         return last;
-
     }
 
     public int getLength() {
--- infinite-stream/src/main/java/com/cognitree/internship/infinite_stream/InfiniteStreamImplementation.java
+++ infinite-stream/src/main/java/com/cognitree/internship/infinite_stream/InfiniteStreamImplementation.java
@@ -4,20 +4,18 @@ import java.util.Scanner;
 
 public class InfiniteStreamImplementation {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-
-        System.out.print("Enter the size of the window: ");
-        int size = sc.nextInt();
-        sc.nextLine();
-
-
-        System.out.println("Enter the numbers: ");
-        String[] str = sc.nextLine().split(" ");
-
-        InfiniteStreamAverage<Double> obj = new InfiniteStreamAverage<>(size);
-
-        for(String s:str){
-            System.out.println(obj.computeRunningAverage(Double.parseDouble(s)));
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        scanner.nextLine();
+        InfiniteStreamAverage<Double> object = new InfiniteStreamAverage<>(size);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine().trim();
+            if (line.isEmpty())
+                break;
+            String[] numbers = line.split(" ");
+            for (String number : numbers) {
+                System.out.println(object.computeRunningAverage(Double.parseDouble(number)));
+            }
         }
     }
 
